f5acf225ca841494f3b798a03abf7eba9f541e9e
wahajullah <50517409+wahajullah@users.noreply.github.com>
2019-06-19 11:10:56 +0500

    tasks

---
+++ 03-Comp_div.py
@@ -0,0 +1,6 @@
+# Ai Assignment 03 - If Numbers are Completely Divisible
+a, b = input("Enter First Number  : "), input("Enter Second Number : ")
+if (int(a) % int(b)) == 0:
+    print("Number is Completely Divisible")
+else:
+    print("No Number is Not Completely Divisible")
